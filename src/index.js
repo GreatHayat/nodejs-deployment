6c34273a2b0becaf6b9ce4f94edb0535954d2eac
great_hayat <greathayat@gmail.com>
2023-01-08 16:36:43 +0500

    disabled mongodb connection

--- src/index.js
+++ src/index.js
@@ -7,10 +7,10 @@ const route = require("./routes");
 const app = express();
 app.use(express.json({ limit: "25mb" }));
 
-mongoose
-  .connect("mongodb://localhost:27017/bootcamp")
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((error) => console.log(`Couldn't connected to mongodb ${error}`));
+// mongoose
+//   .connect("mongodb://localhost:27017/bootcamp")
+//   .then(() => console.log("Connected to MongoDB"))
+//   .catch((error) => console.log(`Couldn't connected to mongodb ${error}`));
 
 app.use("/assets", express.static("uploads"));
 app.use("/api", route);
